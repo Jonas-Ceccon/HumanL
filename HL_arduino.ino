6caafe01a48d2ed78e88bd7ad7e4ec19c7f97e4d
Jonas Ceccon <35008633+Jonas-Ceccon@users.noreply.github.com>
2019-05-15 11:23:28 +0200

    Add files via upload
    Just a non commented and early version of the Arduino program.

---
+++ HL_arduino.ino
@@ -0,0 +1,43 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(40, 6, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial){
+    ;
+  }
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  
+  
+  String recv = Serial.readString();
+  
+  while(recv != "START"){
+    Serial.write("no START marker recieved");
+    String recv = Serial.readString();
+  }
+
+  delay(100);
+  Serial.println("START");
+  delay(100);
+  
+  int lengt = Serial.read();
+  delay(100);
+  
+  Serial.write(lengt);
+  delay(100);
+  
+  if(Serial.readString()=="ERROR"){
+    return;
+  }
+  
+  
+}
