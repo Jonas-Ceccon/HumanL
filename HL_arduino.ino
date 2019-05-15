57ff80524845107e37edf320d3a1166d2a31a344
Jonas Ceccon <35008633+Jonas-Ceccon@users.noreply.github.com>
2019-05-15 12:10:37 +0200

    Update HL_arduino.ino
    Correcting errors and stuff

--- HL_arduino.ino
+++ HL_arduino.ino
@@ -18,14 +18,14 @@ void loop() {
   }
   waitinput(); //wait for serial input
   while(Serial.readString() != "START"){//if the serial input isn't the SRART marker from the pthon program, then do nothing
-    delay(100) //wait for python to be in a 'recieving mood'
+    delay(100); //wait for python to be in a 'recieving mood'
     Serial.write("no START marker recieved"); //send the error code to python
     waitinput(); //wait for another serial input
   }
 
   delay(100); //wait for python to set itself back in order to recieve data
   Serial.println("START"); //tell python that the START marker was recieved
-  waitinput();//wait for python's input
+  waitinput(); //wait for python's input
   
   int lengt = Serial.read(); //this variable sent by python is the total ammount of data that will be sent through serial port (/!\ including the two markers, START and END /!\)
   delay(100);//wait for python
